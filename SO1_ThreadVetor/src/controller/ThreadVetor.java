a85de8418fefd6ab4a159778757ae1ea04fdc91b
Hideki <Hideki@Hideki-PC>
2021-08-30 17:45:54 -0300

    Testes e finalização

--- SO1_ThreadVetor/src/controller/ThreadVetor.java
+++ SO1_ThreadVetor/src/controller/ThreadVetor.java
@@ -1,5 +1,49 @@
 package controller;
 
-public class ThreadVetor {
+public class ThreadVetor extends Thread {
+
+	private int[] vetor;
+	private int verifica;
+
+	public ThreadVetor(int[] vetor, int verifica) {
+		this.vetor = vetor;
+		this.verifica = verifica;
+	}
+
+	@Override 
+	public void run() {
+		if (verifica % 2 == 0) {
+			execFor(); // se for número par ==> execFor()
+		} else {
+			execForeach(); // se for número ímpar ==> execFor()
+		}
+	}
+
+	private void execFor() {
+		double tempoInicial = System.nanoTime();
+
+		for (int i = 0; i < vetor.length; i++) {
+//			System.out.println("Laço For Vetor #" + i + " = " + vetor[i]);
+		}
+
+		double tempoFinal = System.nanoTime();
+		// ns -> s ==> / 10 ^ 9 ==> Math.pow(10, 9)
+		double tempoTotal = (tempoFinal - tempoInicial) / (Math.pow(10, 9));
+		System.out.println("\nTempo c/ estrutura For ==> " + tempoTotal + "s");
+
+	}
+	
+	private void execForeach() {
+		double tempoInicial = System.nanoTime();
+		
+		for (int i: vetor) {
+//			System.out.println("Laço Foreach Vetor #" + i + " = " + vetor[i]);
+		}
+		
+		double tempoFinal = System.nanoTime();
+		// ns -> s ==> / 10 ^ 9 ==> Math.pow(10, 9)
+		double tempoTotal = (tempoFinal - tempoInicial) / (Math.pow(10, 9));
+		System.out.println("\nTempo c/ estrutura Foreach ==> " + tempoTotal + "s");
+	}
 
 }
--- SO1_ThreadVetor/src/view/Principal.java
+++ SO1_ThreadVetor/src/view/Principal.java
@@ -1,5 +1,3 @@
-package view;
-
 // Criar em java um projeto com uma Thread chamada ThreadVetor, 
 // que receba um valor numérico e vetor como parâmetros.
 // Caso o valor numérico seja par, a thread deve percorrer o vetor utilizando uma 
@@ -11,9 +9,36 @@ package view;
 // Deve iniciar 2 ThreadVetor e para uma passar o número 1 e o vetor e para a outra, 
 // passar o número 2 e o mesmo vetor 
 
+package view;
+
+import controller.ThreadVetor;
+
 public class Principal { // Chama as Threads
 	
 	public static void main(String[] args) {
+		
+		// Criar o vetor de 1000 posições
+		int tamanho = 1000;
+		int[] vetor = new int [tamanho];
+		
+		// Gerar números aleatórios
+		// Math.random() gera números entre 0 e 0,999, portanto multiplica por 100 para ter entre 0 e 99
+		// Soma +1 para ter a possibilidade do número 100 e eliminar a possibilidade de ter 0
+		for (int i = 0; i < tamanho ; i++) {
+			int num = (int) ((Math.random() * 100) + 1);
+			vetor[i] = num; // salva o numero gerado de acordo com indice na variavel num
+		}
+		
+		// Classe ThreadVetor
+		// Tratando-se de Threads sempre haverá a possibilidade de iniciar com 1 ou 2, aleatóriamente
+		
+		// Inicia essa Thread com o numero 1 e verifica que é ímpar, logo executa Foreach
+		ThreadVetor tv1 = new ThreadVetor(vetor, 1);
+		tv1.start();
+		
+		// Inicia essa Thread com o numero 2 e verifica que é par, logo executa For
+		ThreadVetor tv2 = new ThreadVetor(vetor, 2);
+		tv2.start();
 		
 	}
 
